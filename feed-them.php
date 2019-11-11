310df79e263e6e93eb7a21aaa93c05790f74f112
Justin Labadie <50183104+justinslickremix@users.noreply.github.com>
2019-11-11 14:07:43 -0500

    Missing comma
    Missing comma ...

--- feed-them.php
+++ feed-them.php
@@ -7,18 +7,18 @@
  * Plugin Name: Feed Them Social - Page, Post, Video and Photo Galleries
  * Plugin URI: https://feedthemsocial.com/
  * Description: Customize feeds for Facebook Pages, Album Photos, Videos & Covers, Instagram, Twitter, Pinterest & YouTube on pages, posts or widgets.
- * Version: 2.7.7.1
+ * Version: 2.7.7.2
  * Author: SlickRemix
  * Author URI: https://www.slickremix.com/
  * Text Domain: feed-them-social
  * Domain Path: /languages
  * Requires at least: WordPress 4.0.0
  * Tested up to: WordPress 5.2.4
- * Stable tag: 2.7.7.1
+ * Stable tag: 2.7.7.2
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  *
- * @version    2.7.7.1
+ * @version    2.7.7.2
  * @package    FeedThemSocial/Core
  * @copyright  Copyright (c) 2012-2019 SlickRemix
  *
@@ -31,7 +31,7 @@
  *
  * Makes sure any js or css changes are reloaded properly. Added to enqued css and js files throughout!
  */
-define( 'FTS_CURRENT_VERSION', '2.7.7.1' );
+define( 'FTS_CURRENT_VERSION', '2.7.7.2' );
 
 define( 'FEED_THEM_SOCIAL_NOTICE_STATUS', get_option( 'rating_fts_slick_notice', false ) );
 
